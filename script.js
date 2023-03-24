15948770a3636b647fdff3c0b4a0ba178a5510b2
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-03-24 23:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+var keys = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
+
 function letterCombinations(input_digit) {
   //Complete the function
+	if(input_digit.length == 0){
+		var base = [];
+		base.push("");
+		return base;
+	}
+
+	var fd = input_digit.charAt(0);
+	var rest = input_digit.substring(1);
+	var small = letterCombinations(rest);
+	var ans = [];
+
+	for (let i = 0; i < keys[fd].length; i++) {
+		for (let j = 0; j < small.length; j++) {
+			var str = keys[fd].charAt(i) + small[j];
+			ans.push(str);
+		}
+	}
+	return ans;
 }
 
 module.exports = letterCombinations;
